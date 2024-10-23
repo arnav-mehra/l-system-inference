1c8c4539a544b0cb6cdc1bf9ff6875533d19b626
ArnavMeh <mehraarn000@gmail.com>
2024-10-23 01:26:16 -0400

    rip

--- cpp-code/hist_to_sol.cpp
+++ cpp-code/hist_to_sol.cpp
@@ -1,14 +1,143 @@
 #include "util.hpp"
+#include <queue>
 
-unordered_map<Symbol, unordered_map<Symbol, int>> hists = {
-    { '*', {{'0', 1}, {'1', 2}, {'2', 1}} },
-    { '0', {{'0', 1}, {'1', 2}, {'2', 1}} },
-    { '1', {{'0', 1}, {'1', 2}, {'2', 1}} },
-    { '2', {{'0', 1}, {'1', 2}, {'2', 1}} }
+#define DEBUG false
+
+struct RuleGen {
+    queue<pair<RuleSet, Histograms>> branches;
+    Histogram rule_lengths;
+    Symbols target;
+
+    RuleGen(Symbols alphabet, Histograms hists, Symbols target) : target(target) {
+        Symbols all_symbols = alphabet;
+        all_symbols.push_back(AXIOM_SYMBOL);
+        
+        // init rule_lengths
+        for (Symbol symbol : all_symbols) {
+            rule_lengths[symbol] = 0;
+            for (auto& [_, cnt] : hists[symbol]) {
+                rule_lengths[symbol] += cnt;
+            }
+            if constexpr (DEBUG) cout << "symbol=" << symbol << " cnt=" << rule_lengths[symbol] << '\n';
+        }
+
+        // init first branch
+        RuleSet rule_set;
+        for (Symbol symbol : all_symbols) {
+            rule_set.map[symbol] = {};
+        }
+        branches.push({rule_set, hists});
+    }
+
+    RuleSet& rule_set() {
+        return branches.front().first;
+    }
+
+    Histograms& hists() {
+        return branches.front().second;
+    }
+
+    void branch() {
+        if constexpr (DEBUG) cout << "dup branch\n";
+        branches.push({ rule_set(), hists() });
+    }
+
+    void try_adding_symbol(const Symbol symbol, int idx_offset, const int depth, int iter_id) {
+        auto& hist = hists()[symbol]; // histogram of remaining symbols.
+        auto& to_symbols = rule_set().map[symbol]; // current rule for symbol.
+
+        for (auto& [ next_symbol, cnt ] : hist) {
+            if (cnt == 0) continue;
+            if constexpr (DEBUG) cout << iter_id << ": next_symbol=" << next_symbol << " cnt=" << cnt << '\n'; 
+
+            // add symbol to rule
+            to_symbols.push_back(next_symbol);
+            hist[next_symbol]--;
+
+            // try added symbol
+            auto [ succ, new_idx_offset ] = iter(next_symbol, idx_offset, depth - 1);
+            
+            // if it worked, duplicate the current {RuleSet, Histograms} context.
+            if (succ) {
+                branch();
+            }
+
+            // remove symbol from rule
+            to_symbols.pop_back();
+            hist[next_symbol]++;
+        }
+    }
+
+    int id = 0;
+    pair<bool, int> iter(const Symbol symbol, int idx_offset, const int depth) {
+        auto& to_symbols = rule_set().map[symbol]; // current rule for symbol.
+        int needed_symbols = rule_lengths[symbol]; // number of symbols the rule needs to have.
+
+        int iter_id = id++;
+        if constexpr (DEBUG) cout << iter_id << ": symbol=" << symbol << " depth=" << depth << " offset=" << idx_offset << " needed_symbols=" << needed_symbols << '\n';
+
+        if (depth == 0) {
+            bool matches = target[idx_offset] == symbol;
+            if constexpr (DEBUG) cout << iter_id << ": matches=" << matches << "\n";
+            return { matches, idx_offset + 1 };
+        }
+
+        for (int i = 0; i < needed_symbols; i++) {
+            if constexpr (DEBUG) cout << iter_id << ": i=" << i << '\n';
+
+            // rule symbol already assigned, just need to check if its valid.
+            if (i < to_symbols.size()) {
+                Symbol next_symbol = to_symbols[i];
+                auto [ succ, new_idx_offset ] = iter(next_symbol, idx_offset, depth - 1);
+                idx_offset = new_idx_offset;
+                if (!succ) return { idx_offset, false };
+            }
+            // rule symbols not assigned, try all possible additions.
+            else {
+                try_adding_symbol(symbol, idx_offset, depth, iter_id);
+                return { idx_offset, false };
+            }
+        }
+
+        // passed on all letters, return true.
+        return { idx_offset, true };
+    }
+
+    pair<bool, RuleSet> run(int depth) {
+        while (branches.size()) {
+            if constexpr (DEBUG) cout << "\ntrying next branch:\n";
+            id = 0;
+            auto [_, succ] = iter(AXIOM_SYMBOL, 0, depth);
+            if (succ) {
+                return { true, rule_set() };
+            }
+            branches.pop();
+        }
+        return { false, RuleSet() };
+    }
 };
 
-void iter(int depth, )
-
 int main() {
+    const Symbols alphbet = { '0', '1', '2' };
 
+    const Histograms hists = {
+        { AXIOM_SYMBOL, {{'0', 1}, {'1', 1}, {'2', 0}} },
+        { '0', {{'0', 0}, {'1', 1}, {'2', 0}} },
+        { '1', {{'0', 1}, {'1', 0}, {'2', 1}} },
+        { '2', {{'0', 0}, {'1', 1}, {'2', 0}} }
+    };
+
+    const int depth = 2;
+
+    vector<Symbols> targets = {
+        { '0', '1', '2', '1' },
+        { '1', '1', '0', '2' }
+    };
+
+    for (auto target : targets) {
+        RuleGen gen(alphbet, hists, target);
+        auto [succ, rule_set] = gen.run(depth);
+        cout << string(rule_set);
+        cout << succ << '\n';
+    }
 }
--- python-code/main.py
+++ python-code/main.py
@@ -20,34 +20,34 @@ def mat_mult(symbols: list, m1: str, m2: str, m3: str):
             conds.append(result_rc == dot_prod)
     return And(conds)
 
-def mat_vec_mult(symbols: list, m1: str, v1: str, v2: str):
+def mat_vec_mult(symbols: list, vi: str, m: str, vf: str):
     conds = []
-    for row_symbol in symbols:
+    for col_symbol in symbols:
         mult_ops = []
         for iter_symbol in symbols:
-            m1_ri = get_mat_id(m1, row_symbol, iter_symbol)
-            v1_i0 = get_mat_id(v1, iter_symbol, 0)
-            mult_ops.append(m1_ri * v1_i0)
+            vi_i = get_mat_id(vi, 0, iter_symbol)
+            m_ri = get_mat_id(m, iter_symbol, col_symbol)
+            mult_ops.append(m_ri * vi_i)
         dot_prod = Sum(mult_ops)
 
-        result_rc = get_mat_id(v2, row_symbol, 0)
-        conds.append(result_rc == dot_prod)
+        vf_c = get_mat_id(vf, 0, col_symbol)
+        conds.append(vf_c == dot_prod)
     return And(conds)
 
 def vec_eq(symbols: list, v1: str, v2: str):
     conds = []
     for iter_symbol in symbols:
-        v1_i0 = get_mat_id(m1, iter_symbol, 0)
-        v2_i0 = get_mat_id(v1, iter_symbol, 0)
-        conds.append(v1_i0 == v2_i0)
+        v1_i = get_mat_id(v1, 0, iter_symbol)
+        v2_i = get_mat_id(v2, 0, iter_symbol)
+        conds.append(v1_i == v2_i)
     return And(conds)
 
 def hist_eq(symbol_hist: Counter, v: str):
     hist_conds = []
     for symbol in symbol_hist:
         count = symbol_hist[symbol]
-        v1_i0 = get_mat_id(v, symbol, 0)
-        hist_conds.append(v1_i0 == count)
+        v_i = get_mat_id(v, 0, symbol)
+        hist_conds.append(v_i == count)
     return And(hist_conds)
 
 def cost(symbols: list, m: str, v: str):
@@ -57,44 +57,82 @@ def cost(symbols: list, m: str, v: str):
             m_rc = get_mat_id(m, row_symbol, col_symbol)
             vars.append(m_rc)
     for symbol in symbols:
-        v_r0 = get_mat_id(v, symbol, 0)
-        vars.append(v_r0)
+        v_r = get_mat_id(v, 0, symbol)
+        vars.append(v_r)
     return Sum(vars)
 
-def solve(goal: list):
+# note: to use m_prefix + "^1" should already exist.
+def mat_pow(symbols: list, m_prefix: str, p: int):
+    if p == 1:
+        return And([])
+
+    conds = []
+
+    # compute m^(p/2)
+    p_half = p // 2
+    m_p_half_cond = mat_pow(symbols, m_prefix, p_half)
+    conds.append(m_p_half_cond)
+
+    # m^(p/2*2) = m^(p/2) * m^(p/2)
+    m_p_half = m_prefix + "^" + str(p_half)
+    m_p = m_prefix + "^" + str(p_half * 2)
+    m_p_cond = mat_mult(symbols, m_p_half, m_p_half, m_p)
+    conds.append(m_p_cond)
+
+    # m^p = m^(p/2) * m^(p/2) * m^1 (if p is odd)
+    if p_half * 2 != p:
+        m_1 = m_prefix + "^1"
+        m_p_adj = m_prefix + "^" + str(p)
+        m_p_adj_cond = mat_mult(symbols, m_p, m_1, m_p_adj)
+        conds.append(m_p_adj_cond)
+
+    return And(conds)
+
+def solve(goal: list, p: int):
     symbols = list(set(goal))
     symbol_hist = Counter(goal)
 
     new_conds = []
 
-    # m1 = m0^2
-    mat_mult_cond = mat_mult(symbols, "m0", "m0", "m1")
+    # m^p
+    m_p = "m^" + str(p)
+    mat_mult_cond = mat_pow(symbols, "m", p)
     new_conds.append(mat_mult_cond)
 
-    # v1 = m1 * v0
-    mat_vec_mult_cond = mat_vec_mult(symbols, "m1", "v0", "v1")
+    # vf = vi * m^p
+    mat_vec_mult_cond = mat_vec_mult(symbols, "vi", m_p, "vf")
     new_conds.append(mat_vec_mult_cond)
+    print(mat_vec_mult_cond)
 
-    # v1 = histogram(symbols)
-    hist_cond = hist_eq(symbol_hist, "v1")
+    # vf = histogram(symbols)
+    hist_cond = hist_eq(symbol_hist, "vf")
     new_conds.append(hist_cond)
 
-    # 0 <= m0[r][c] <= hist[r]
+    # 0 <= m^1[r][c] <= hist[r]
     for from_symbol in symbols:
         for to_symbol in symbols:
-            cell_id = get_mat_id("m0", from_symbol, to_symbol)
+            cell_id = get_mat_id("m^1", from_symbol, to_symbol)
             new_conds.append(cell_id >= 0)
             new_conds.append(cell_id <= symbol_hist[from_symbol])
-    
-    # 0 <= v0[r] <= hist[r]
+
+    # 0 <= vi[r] <= hist[r]
     for symbol in symbols:
-        cell_id = get_mat_id("v0", symbol, 0)
+        cell_id = get_mat_id("vi", 0, symbol)
         new_conds.append(cell_id >= 0)
         new_conds.append(cell_id <= symbol_hist[symbol])
 
-    # cost = sum_rc(m0[r][c]) + sum_r(v[r])
-    cost_expr = cost(symbols, "m0", "v0")
-    # new_conds.append(cost_expr < 4)
+    # 1 <= sum_c(m^1[r][c]) <= hist[r]
+    for from_symbol in symbols:
+        terms = []
+        for to_symbol in symbols:
+            cell_id = get_mat_id("m^1", from_symbol, to_symbol)
+            terms.append(cell_id)
+        to_sum = Sum(terms)
+        new_conds.append(to_sum >= 1)
+        new_conds.append(to_sum <= symbol_hist[from_symbol])
+
+    # cost = sum_rc(m^1[r][c]) + sum_r(v[r])
+    cost_expr = cost(symbols, "m^1", "vi")
 
     solver = Optimize()
     solver.add(new_conds)
@@ -102,11 +140,28 @@ def solve(goal: list):
 
     if solver.check() == sat:
         print("sat")
-        print(solver.model())
+
+        model = solver.model()
+        print(model)
+        
+        rule_hists = dict()
+        for from_symbol in symbols:
+            hist = dict()
+            for to_symbol in symbols:
+                m1_rc = get_mat_id("m^1", from_symbol, to_symbol)
+                hist[to_symbol] = model[m1_rc]
+            rule_hists[from_symbol] = hist
+        print(rule_hists)
+
+        axiom_hist = dict()
+        for symbol in symbols:
+            v_rc = get_mat_id("vi", 0, symbol)
+            axiom_hist[symbol] = model[v_rc]
+        print(axiom_hist)
     else:
         print("not sat")
     
     return solver
 
 if __name__ == '__main__':
-    solve([0, 1, 2, 1])
+    solve([0, 1, 2, 1], 2)
