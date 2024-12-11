e8d712d4e5f7eee2ba8044a585a4ed93cc43800f
ArnavMeh <mehraarn000@gmail.com>
2024-12-11 13:13:44 -0500

    minor debug test fixes

--- cpp-code/brute_force.cpp
+++ cpp-code/brute_force.cpp
@@ -1,15 +1,22 @@
 #include "brute_force.hpp"
+#include "brute_force_pruned.hpp"
 #include "gen_data.hpp"
+#include <chrono>
 
 int main() {
-    auto depth_range = Range(4, 4);
+    auto depth_range = Range(5, 5);
     auto alphabet_range = Range(4, 4);
-    auto complexity_range = Range(2, 2);
+    auto complexity_range = Range(3, 3);
 
     auto data_gen = DataGen(depth_range, alphabet_range, complexity_range);
     auto [iter, data] = data_gen.gen();
     cout << string(data);
 
-    auto [ status, rule_set ] = BF::solver(data.alphabet_size, data.depth, data.target);
-    printSolverResult(status, rule_set);
+    auto start = std::chrono::high_resolution_clock::now();
+    auto [ status, rule_set ] = BF::solver(data.alphabet_size, data.depth, data.target, 30);
+    auto end = std::chrono::high_resolution_clock::now();
+    print_solver_result(status, rule_set);
+
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Elapsed time: " << elapsed.count() << " seconds" << std::endl;
 }
--- cpp-code/gen_data.cpp
+++ cpp-code/gen_data.cpp
@@ -1,18 +1,36 @@
 #include "gen_data.hpp"
+#include <fstream>
+#include <sstream>
 
 int main() {
-    auto depth_range = Range(4, 4);
-    auto alphabet_range = Range(4, 4);
-    auto complexity_range = Range(6, 6);
+    // auto depth_range = Range(4, 4);
+    // auto alphabet_range = Range(4, 4);
+    // auto complexity_range = Range(6, 6);
 
-    auto data_gen = DataGen(depth_range, alphabet_range, complexity_range);
-    auto [iter, data] = data_gen.gen();
-    cout << "iters: " << iter << '\n';
-    cout << string(data);
+    // auto data_gen = DataGen(depth_range, alphabet_range, complexity_range);
+    // auto [iter, data] = data_gen.gen();
+    // cout << "iters: " << iter << '\n';
+    // cout << string(data);
 
-    auto h = Histogram(data.alphabet_size);
-    h.digest(data.target);
-    cout << string(h);
+    // auto h = Histogram(data.alphabet_size);
+    // h.digest(data.target);
+    // cout << string(h);
 
     // cout << data.rule_set.produces(data.target, 4);
+
+    ifstream file("../outData");
+
+    vector<int> values;
+    string line, value;
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        while (getline(ss, value, ',')) {
+            values.push_back(stoi(value));
+        }
+    }
+
+    for (int x : values) {
+        cout << x << ' ';
+    } cout << '\n';
 }
